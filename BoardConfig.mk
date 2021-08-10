9aa2b6720a8ea9b4bbd8bf31814c77a983727821
George <geoster3d@gmail.com>
2021-08-10 10:57:25 +0300

    phoenix: Import system/vendor props from stock
    * Categorize, alphabetize and clean up unused props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,10 @@ TARGET_USES_MKE2FS := true
 
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
