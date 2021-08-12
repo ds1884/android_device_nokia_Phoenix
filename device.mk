982d3113acefbfd6e85524b78000c34dcdd3a982
zskynet09 <hakimfbacc@gmail.com>
2021-08-12 20:28:46 +0530

    Initial bringup of Phoenix

---
+++ device.mk
@@ -0,0 +1,37 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/nokia/Phoenix/Phoenix-vendor.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+# IDC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/goodix_fp.idc:system/usr/idc/goodix_fp.idc
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/goodix_fp.kl:system/usr/keylayout/goodix_fp.kl
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
---
+++ lineage_Phoenix.mk
@@ -0,0 +1,22 @@
+# Device Stuff
+$(call inherit-product, device/nokia/Phoenix/device.mk)
+
+# Inherit some common LineageOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_Phoenix
+PRODUCT_DEVICE := Phoenix
+PRODUCT_BRAND := Nokia
+PRODUCT_MODEL := Nokia 8.1
+PRODUCT_MANUFACTURER := Nokia
+
+BUILD_FINGERPRINT := "Nokia/Phoenix_00WW/PNX_sprout:11/RKQ1.200906.002/00WW_6_210:user/release-key"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="PNX_00WW_FIH-user 11 00WW_6_210 00WW_6_210 release-keys" \
+    PRODUCT_NAME="Phoenix_00WW" \
+    PRODUCT_DEVICE="PNX_sprout"
+
+PRODUCT_GMS_CLIENTID_BASE := android-nokia
