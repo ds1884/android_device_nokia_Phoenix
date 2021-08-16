ec36737a44ef1d2fc91854f0272a9dd864074587
zskynet09 <hakimfbacc@gmail.com>
2021-08-16 11:17:27 +0530

    Add framework detect modules to PRODUCT_PACKAGES Add JNI and libqti_vndfwk_detect module name to product_packages to specify module to be installed to device.

--- device.mk
+++ device.mk
@@ -62,6 +62,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
+# Framework detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
